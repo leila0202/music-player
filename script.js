03f8a3a51f6f9be1646bbc979e466490811d6d0a
Leila Zrim <leilazrim@gmail.com>
2024-01-12 14:51:53 +0100

    [js - 03] [Step 41] Calculate the next song and play it

--- script.js
+++ script.js
@@ -112,6 +112,8 @@ const playNextSong = () => {
     playSong(userData?.songs[0].id);
   } else {
     const currentSongIndex = getCurrentSongIndex();
+    const nextSong = userData?.songs[currentSongIndex + 1];
+    playSong(nextSong.id);
   }
 };
 
