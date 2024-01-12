b1ad2f07bdfe06cd5c4c748bc11ca52188d7b153
Leila Zrim <leilazrim@gmail.com>
2024-01-12 14:59:55 +0100

    [js - 03] [Step 44] Added implementation for "playPreviousSong"

--- script.js
+++ script.js
@@ -117,7 +117,13 @@ const playNextSong = () => {
   }
 };
 
-const playPreviousSong = () => {};
+const playPreviousSong = () => {
+  if (userData?.currentSong === null) {
+    return;
+  } else {
+    const currentSongIndex = getCurrentSongIndex();
+  }
+};
 
 const renderSongs = (array) => {
   const songsHTML = array
