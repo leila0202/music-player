e409f736f207399ed175a26a61f0ecc3156c0124
Leila Zrim <leilazrim@gmail.com>
2024-01-12 09:39:18 +0100

    [js - 03][Step 03] Added empty array for storing the songs

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ const pauseButton = document.getElementById("pause");
 const nextButton = document.getElementById("next");
 const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
+
+const allSongs = [];
