9b05f14e1b519b7cd1a4231d9b573fbdee92741b
Leila Zrim <leilazrim@gmail.com>
2024-01-12 10:12:16 +0100

    [js - 03][Step 04] Add an object to the "allSongs" array with one entry

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ const nextButton = document.getElementById("next");
 const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
 
-const allSongs = [];
+const allSongs = [
+  {
+    id: 0,
+    title: "Scratching The Surface",
+    artist: "Quincy Larson",
+    duration: "4:25",
+    src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/scratching-the-surface.mp3",
+  },
+];
