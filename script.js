cf414e49be2f22430c5016bbdc31269e1920dafe
Leila Zrim <leilazrim@gmail.com>
2024-01-15 11:17:24 +0100

    [js - 03] [Step 55] Get the current artist and title

--- script.js
+++ script.js
@@ -131,6 +131,8 @@ const playPreviousSong = () => {
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
   const songArtist = document.getElementById("player-song-artist");
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
 };
 
 const highlightCurrentSong = () => {
