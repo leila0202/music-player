fe590ce793e7af5f28b8ffa7bc808c31b1cd5c8a
Leila Zrim <leilazrim@gmail.com>
2024-01-15 11:34:58 +0100

    [js - 03] [Step 62] add "shuffle" arrow function definition

--- script.js
+++ script.js
@@ -130,6 +130,8 @@ const playPreviousSong = () => {
   }
 };
 
+const shuffle = () => {};
+
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
   const songArtist = document.getElementById("player-song-artist");
