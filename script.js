e4e74a8573119d4a7b48fae6669d067cbf5a8357
Leila Zrim <leilazrim@gmail.com>
2024-01-15 11:10:05 +0100

    [js - 03] [Step 51] Add the "aria current" attribute to the currently playing song

--- script.js
+++ script.js
@@ -135,6 +135,9 @@ const highlightCurrentSong = () => {
   playlistSongElements.forEach((songEl) => {
     songEl.removeAttribute("aria-current");
   });
+  if (songToHighlight) {
+    songToHighlight.setAttribute("aria-current", "true");
+  }
 };
 
 const renderSongs = (array) => {
