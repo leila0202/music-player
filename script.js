2ca7d7d4e2f099c54c23c26cc3d570820b9a41b4
Leila Zrim <leilazrim@gmail.com>
2024-01-15 13:17:41 +0100

    [js - 03] [Step 79] Assign the original songs to the "userData" songs

--- script.js
+++ script.js
@@ -160,7 +160,9 @@ const deleteSong = (id) => {
     resetButton.appendChild(resetText);
     playlistSongs.appendChild(resetButton);
 
-    resetButton.addEventListener("click", () => {});
+    resetButton.addEventListener("click", () => {
+      userData.songs = [...allSongs];
+    });
   }
 };
 
