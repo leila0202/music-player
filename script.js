f9d2594378aa168748cd94e99f05a9cec31d4c49
Leila Zrim <leilazrim@gmail.com>
2024-01-15 11:37:45 +0100

    [js - 03] [Step 63] Shuffle the songs array

--- script.js
+++ script.js
@@ -130,7 +130,9 @@ const playPreviousSong = () => {
   }
 };
 
-const shuffle = () => {};
+const shuffle = () => {
+  userData?.songs.sort(() => Math.random() - 0.5);
+};
 
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
