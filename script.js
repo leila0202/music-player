2610c34db01f407a133b678d9a0067e9c8c08a22
Leila Zrim <leilazrim@gmail.com>
2024-01-15 13:07:16 +0100

    [js - 03] [Step 74] Create reset button

--- script.js
+++ script.js
@@ -153,6 +153,7 @@ const deleteSong = (id) => {
   highlightCurrentSong();
   setPlayButtonAccessibleText();
   if (userData.songs.length === 0) {
+    const resetButton = document.createElement("button");
   }
 };
 
