e5faa3c5b962c7458cf590cd1067d0af3b97fea1
Leila Zrim <leilazrim@gmail.com>
2024-01-15 11:01:46 +0100

    [js - 03] [Step 48] Get the currently playing song by id

--- script.js
+++ script.js
@@ -129,6 +129,9 @@ const playPreviousSong = () => {
 
 const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll(".playlist-song");
+  const songToHighlight = document.getElementById(
+    `song-${userData?.currentSong?.id}`
+  );
 };
 
 const renderSongs = (array) => {
