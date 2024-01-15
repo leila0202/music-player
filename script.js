61b19a4d624f992cfb042b58dd520d1e227581a2
Leila Zrim <leilazrim@gmail.com>
2024-01-15 11:32:28 +0100

    [js - 03] [Step 60] Set "aria-label" to the currently playing song or the first song

--- script.js
+++ script.js
@@ -176,6 +176,10 @@ const renderSongs = (array) => {
 
 const setPlayButtonAccessibleText = () => {
   const song = userData?.currentSong || userData?.songs[0];
+  playButton.setAttribute(
+    "aria-label",
+    song?.title ? `Play ${song.title}` : "Play"
+  );
 };
 
 const getCurrentSongIndex = () => {
