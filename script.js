ad12b5a978590b5bd8f3e02fa74387d4cd2ca1cf
Leila Zrim <leilazrim@gmail.com>
2024-01-15 11:56:45 +0100

    [js - 03] [Step 67] Add "deleteSong" definition

--- script.js
+++ script.js
@@ -140,6 +140,8 @@ const shuffle = () => {
   setPlayButtonAccessibleText();
 };
 
+const deleteSong = (id) => {};
+
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
   const songArtist = document.getElementById("player-song-artist");
