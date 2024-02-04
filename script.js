5cbe2bf35393fe1df6a526a54bb06da90c5b8ec0
Leila <leilazrim@gmail.com>
2024-02-04 04:31:23 +0100

    [js - 03] [Step 24 new] Add second part of the sort callback

--- script.js
+++ script.js
@@ -117,6 +117,10 @@ const deleteSong = (id) => {
     if (a.title < b.title) {
       return -1;
     }
+
+    if (a.title > b.title) {
+      return 1;
+    }
   });
   renderSongs(userData?.songs);
   highlightCurrentSong();
